93ab531f702f57fd02204b7882b68d34539ef08d
Manya Bhardwaj <manyab485@gmail.com>
2025-04-05 16:41:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
+
 provider "aws" {
   region = "us-east-1"
 }
 
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "example" {
-  bucket = "medusa-demo-bucket-1234"
+  bucket = "medusa-demo-bucket-${random_id.bucket_id.hex}"
   acl    = "private"
 }
