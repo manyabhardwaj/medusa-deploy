193f51d41cfbb5505fff84907e63395cc358705c
Manya <manyab485@gmail.com>
2025-04-05 17:12:04 +0530

    Update files

--- main.tf
+++ main.tf
@@ -1,21 +1,78 @@
-
 provider "aws" {
   region = "us-east-1"
 }
 
+# Generate random ID for S3 bucket name uniqueness
 resource "random_id" "bucket_id" {
   byte_length = 4
 }
 
+# S3 Bucket
 resource "aws_s3_bucket" "example" {
   bucket = "medusa-demo-bucket-${random_id.bucket_id.hex}"
   acl    = "private"
 }
 
+# VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Public Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+# Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Associate Route Table with Subnet
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Security Group for port 9000
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow port 9000"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ECS Cluster
 resource "aws_ecs_cluster" "medusa_cluster" {
   name = "medusa-cluster"
 }
 
+# ECS Task Definition
 resource "aws_ecs_task_definition" "medusa_task" {
   family                   = "medusa-task"
   requires_compatibilities = ["FARGATE"]
@@ -34,6 +91,7 @@ resource "aws_ecs_task_definition" "medusa_task" {
   }])
 }
 
+# ECS Fargate Service
 resource "aws_ecs_service" "medusa_service" {
   name            = "medusa-service"
   cluster         = aws_ecs_cluster.medusa_cluster.id
@@ -42,8 +100,15 @@ resource "aws_ecs_service" "medusa_service" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = [aws_subnet.public_subnet.id]
+    subnets          = [aws_subnet.public_subnet.id]
     assign_public_ip = true
-    security_groups = [aws_security_group.allow_http.id]
+    security_groups  = [aws_security_group.allow_http.id]
   }
+
+  depends_on = [aws_ecs_cluster.medusa_cluster]
+}
+
+# Medusa Docker Image Variable
+variable "medusa_image" {
+  default = "medusajs/medusa:v1.8.2"
 }
