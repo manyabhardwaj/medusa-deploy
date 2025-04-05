dfdbb0417ed86211dc32d48963cc62a7a9a2f86c
Manya <manyab485@gmail.com>
2025-04-05 17:04:37 +0530

    Update files

--- main.tf
+++ main.tf
@@ -11,3 +11,39 @@ resource "aws_s3_bucket" "example" {
   bucket = "medusa-demo-bucket-${random_id.bucket_id.hex}"
   acl    = "private"
 }
+
+resource "aws_ecs_cluster" "medusa_cluster" {
+  name = "medusa-cluster"
+}
+
+resource "aws_ecs_task_definition" "medusa_task" {
+  family                   = "medusa-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([{
+    name      = "medusa"
+    image     = var.medusa_image
+    essential = true
+    portMappings = [{
+      containerPort = 9000
+      hostPort      = 9000
+    }]
+  }])
+}
+
+resource "aws_ecs_service" "medusa_service" {
+  name            = "medusa-service"
+  cluster         = aws_ecs_cluster.medusa_cluster.id
+  task_definition = aws_ecs_task_definition.medusa_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [aws_subnet.public_subnet.id]
+    assign_public_ip = true
+    security_groups = [aws_security_group.allow_http.id]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "medusa_image" {
+  default = "medusajs/medusa"
+}
+
